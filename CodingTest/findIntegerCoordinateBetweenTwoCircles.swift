45c808554194289d5e2dbb4319289c0cc57657c6
eensungkim <eensungkim@gmail.com>
2024-02-01 10:50:20 +0900

    docs: 문제 해결을 위한 주석 추가

--- CodingTest/findIntegerCoordinateBetweenTwoCircles.swift
+++ CodingTest/findIntegerCoordinateBetweenTwoCircles.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+// 원점(0, 0)에서 (x, y) 좌표까지의 거리는 x^2 + y^2 = 거리^2 이므로 x^2 + y^2 를 squareRoot 한 값
+// 이 거리가 큰 원의 반지름보다 작거나 같고, 작은 원의 반지름보다는 크거나 같아야 함.
+// 이 개념을 기반으로 하나의 사분면에 있는 점들을 계산한 뒤에 이 값을 4배로 곱하면 됨.
+
 func findIntegerCoordinateBetweenTwoCircles(_ r1:Int, _ r2:Int) -> Int64 {
     var result: Int64 = 0
     let doubleR1 = Double(r1)
