f8d707122cd6fc07abb8e7afca94e3bbaca76508
eensungkim <eensungkim@gmail.com>
2024-04-09 17:36:49 +0900

    feat: 귤 고르기
    https://school.programmers.co.kr/learn/courses/30/lessons/138476

---
+++ CodingTest/pickTangerine.swift
@@ -0,0 +1,29 @@
+//
+//  pickTangerine.swift
+//  CodingTest
+//
+//  Created by Kim EenSung on 4/9/24.
+//
+
+import Foundation
+
+// Dictionary 초기화자를 활용하는 분이 있어서 참고
+// grouping 초기화자를 사용할 경우 중복된 값을 배열의 형태로 정리해줌
+
+func pickTangerine(_ k:Int, _ tangerine:[Int]) -> Int {
+    var sorted = Dictionary(grouping: tangerine) { $0 }.values
+        .sorted { $0.count > $1.count }
+    
+    var k: Int = k
+    var result: Int = 0
+    
+    for item in sorted {
+        k -= item.count
+        result += 1
+        if k <= 0 {
+            return result
+        }
+    }
+    
+    return result
+}
