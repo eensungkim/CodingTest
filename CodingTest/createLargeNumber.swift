389b8e0c6d4fef5574303b9c40d3aa7861f5301f
eensungkim <eensungkim@gmail.com>
2024-06-24 20:00:53 +0900

    feat: 큰 수 만들기
    스택을 활용해 탐욕법으로 접근한다는 개념을 떠올릴 수 있어야 하는게 어려웠던 것 같음
    https://school.programmers.co.kr/learn/courses/30/lessons/42883

---
+++ CodingTest/createLargeNumber.swift
@@ -0,0 +1,34 @@
+//
+//  createLargeNumber.swift
+//  CodingTest
+//
+//  Created by Kim EenSung on 6/24/24.
+//
+
+import Foundation
+
+func createLargeNumber(_ number:String, _ k:Int) -> String {
+    var stack = [Character]()  // 결과를 저장할 스택
+    var removeCount = k        // 제거할 숫자의 개수
+
+    for digit in number {
+        // 스택이 비어 있지 않고, 제거할 수 있는 숫자가 남아 있고, 스택의 마지막 숫자가 현재 숫자보다 작으면
+        while !stack.isEmpty && removeCount > 0 && stack.last! < digit {
+            stack.removeLast() // 스택에서 마지막 숫자 제거
+            removeCount -= 1   // 제거할 숫자 카운트 감소
+            print("스택에서 제거 : ", stack)
+            print("count 의 변화 : ", removeCount)
+        }
+        stack.append(digit) // 현재 숫자를 스택에 추가
+        print("처음 for 문에서 : ", stack)
+    }
+    
+    // 아직 제거할 숫자가 남아 있다면 뒤에서부터 제거
+    while removeCount > 0 {
+        stack.removeLast()
+        removeCount -= 1
+    }
+
+    // 스택을 문자열로 변환하여 결과 반환
+    return String(stack)
+}
